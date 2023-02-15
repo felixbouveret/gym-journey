4108bb1c74f6c0df662318e349aa26a057bae9a0
Félix Bouveret <felix@getluko.com>
2023-02-15 17:56:53 +0100

    Acore: add dotenv

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'prettier'],
-  plugins: ['simple-import-sort', 'autofix'],
+  extends: ["@react-native-community", "prettier"],
+  plugins: ["simple-import-sort", "autofix"],
   rules: {
-    'react-hooks/exhaustive-deps': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'autofix/no-unused-vars': 'error',
-    curly: ['off'],
+    "react-hooks/exhaustive-deps": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "react/react-in-jsx-scope": "off",
+    "autofix/no-unused-vars": "error",
+    curly: ["off"],
   },
   settings: {
-    'import/resolver': {
-      'babel-module': {
+    "import/resolver": {
+      "babel-module": {
         alias: {
-          app: './src/',
+          app: "./src/",
         },
       },
     },
--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -7,6 +7,7 @@ export default function ModalScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
+      <Text>Test : {process.env.TEST}</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
