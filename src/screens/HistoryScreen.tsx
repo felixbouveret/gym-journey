ab2f900b36385516df7a02e916bcfbf87adefdba
Félix Bouveret <felix@getluko.com>
2023-02-28 19:34:56 +0100

    fix: add history screen placeholder

--- src/screens/HistoryScreen.tsx
+++ src/screens/HistoryScreen.tsx
@@ -1,7 +1,22 @@
-import { Text } from "native-base";
+import { Box, Text, VStack } from 'native-base';
 
-import { RootTabScreenProps } from "@/types";
+import { RootTabScreenProps } from '@/types';
 
-export default function HistoryScreen({}: RootTabScreenProps<"History">) {
-  return <Text>History</Text>;
+export default function HistoryScreen({}: RootTabScreenProps<'History'>) {
+  return (
+    <VStack w="full" h="full" p={4} justifyContent="center">
+      <Box
+        rounded={8}
+        p={4}
+        backgroundColor="gray.200"
+        borderColor={'gray.400'}
+        borderStyle="dashed"
+        borderWidth={2}
+      >
+        <Text color={'gray.500'} fontSize="xl" textAlign={'center'}>
+          HistoryScreen in progress...
+        </Text>
+      </Box>
+    </VStack>
+  );
 }
