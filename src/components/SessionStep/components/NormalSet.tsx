5f933a7606d25bccb811a7e22701e0df421d71c1
Félix Bouveret <felix@getluko.com>
2023-03-07 18:52:06 +0100

    Clean step block

---
+++ src/components/SessionStep/components/NormalSet.tsx
@@ -0,0 +1,37 @@
+import { HStack, Text, VStack } from 'native-base';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '@/store';
+import { ProgramSessionStep } from '@/store/Programs';
+
+interface NormalSetProps {
+  item: ProgramSessionStep;
+}
+
+export default function NormalSet({ item }: NormalSetProps) {
+  const { exercices } = useSelector((state: RootState) => state.exercices);
+  const exercice = item.exercices[0];
+  return (
+    <VStack flex="1" space="2">
+      <HStack alignItems="center" justifyContent={'space-between'} space="2">
+        <Text fontSize={'lg'} fontWeight="medium" lineBreakMode="head" numberOfLines={1} flex="1">
+          {exercices.find((exerciceItem) => exerciceItem.id === exercice.exerciceId)?.name}
+        </Text>
+        <Text fontSize={'lg'}>{exercice.weight} kg</Text>
+      </HStack>
+      <HStack space={8} justifyContent={'space-between'}>
+        <HStack space={4} justifyContent={'space-between'}>
+          <Text color={'gray.500'} fontSize={'sm'}>
+            {item.setNumber} séries
+          </Text>
+          <Text color={'gray.500'} fontSize={'sm'}>
+            {exercice.reps} répétitions
+          </Text>
+        </HStack>
+        <Text color={'gray.500'} fontSize={'sm'}>
+          {item.restTime} min
+        </Text>
+      </HStack>
+    </VStack>
+  );
+}
--- src/stacks/Exercices/ExercicesList.tsx
+++ src/stacks/Exercices/ExercicesList.tsx
@@ -1,19 +1,10 @@
 import { Ionicons } from '@expo/vector-icons';
-import {
-  Badge,
-  Button,
-  HStack,
-  Icon,
-  IconButton,
-  Pressable,
-  ScrollView,
-  Text,
-  VStack
-} from 'native-base';
+import { Badge, Button, HStack, Icon, ScrollView, Text, VStack } from 'native-base';
 import { ActionSheetIOS } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 import BlockPlaceholder from '@/components/BlockPlaceholder';
+import ExerciceContainer from '@/components/ExerciceContainer';
 import { RootState } from '@/store';
 import { removeExercice } from '@/store/Exercices';
 import { ExercicesTabScreenProps } from '@/types';
@@ -38,52 +29,29 @@ export default function ExercicesList({ navigation }: ExercicesTabScreenProps<'E
     );
 
   const exercicesList = exercices?.map((item, index) => (
-    <Pressable
+    <ExerciceContainer
       key={index}
-      w="full"
-      flex="1"
       onPress={() => navigation.navigate('ExerciceSingle', { id: item.id })}
+      onOptions={() => onOptions(item.id)}
+      small
     >
-      <HStack
-        space="4"
-        p={4}
-        pr="2"
-        flex="1"
-        backgroundColor={'white'}
-        rounded={8}
-        w="full"
-        alignItems={'center'}
-      >
-        <VStack flex="1" space="2">
-          <HStack alignItems="center" justifyContent={'space-between'}>
-            <Text fontSize={'xl'} fontWeight="medium">
-              {item.name}
-            </Text>
-            {item.isUnilateral && (
-              <Badge>
-                <Text>Unilatéral</Text>
-              </Badge>
-            )}
-          </HStack>
-        </VStack>
-        <IconButton
-          size="sm"
-          p={1}
-          onPress={() => onOptions(item.id)}
-          _icon={{
-            as: Ionicons,
-            color: 'gray.700',
-            name: 'ellipsis-vertical'
-          }}
-        />
+      <HStack alignItems="center" justifyContent={'space-between'}>
+        <Text fontSize={'md'} fontWeight="medium">
+          {item.name}
+        </Text>
+        {item.isUnilateral && (
+          <Badge>
+            <Text>Unilatéral</Text>
+          </Badge>
+        )}
       </HStack>
-    </Pressable>
+    </ExerciceContainer>
   ));
 
   return (
     <VStack h="full" justifyContent={exercices?.length ? '' : 'flex-end'}>
       <ScrollView w="full" h="full">
-        <VStack h="full" space={4} p="4">
+        <VStack h="full" space={2} p="4">
           {!!exercices?.length && exercicesList}
         </VStack>
       </ScrollView>
