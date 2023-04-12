5de7d81b2f2f3c800d35b9e26d507e09d3d618ed
Félix Bouveret <felix@getluko.com>
2023-04-12 21:15:42 +0200

    fix: add expo semantic release

--- release.config.js
+++ release.config.js
@@ -4,16 +4,19 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     { '@semantic-release/changelog': { changelogFile: 'CHANGELOG.md' } },
     '@semantic-release/release-notes-generator',
+    'semantic-release-expo',
     '@semantic-release/github',
     [
       '@semantic-release/npm',
       {
+        assets: ['CHANGELOG.md', 'package.json', 'app.json'],
         npmPublish: false
       }
     ],
     [
       '@semantic-release/git',
       {
+        assets: ['CHANGELOG.md', 'package.json', 'app.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
