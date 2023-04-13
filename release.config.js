be587f7ddeb3eddaac5f1ae72d885390e1a79b99
Félix Bouveret <felix@getluko.com>
2023-04-13 16:58:52 +0200

    Fix semantic release

--- release.config.js
+++ release.config.js
@@ -5,16 +5,16 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     [
-      '@semantic-release/exec',
-      {
-        successCmd: 'node ./script/release/index.js "${nextRelease.version}"'
-      }
-    ],
-    [
       '@semantic-release/npm',
       {
         assets: ['package.json', 'app.config.js'],
         npmPublish: false
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        successCmd: 'node ./script/release/index.js "${nextRelease.version}"'
       }
     ],
     { '@semantic-release/changelog': { changelogFile: 'CHANGELOG.md' } },
