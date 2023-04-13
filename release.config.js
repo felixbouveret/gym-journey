efc956db1b946d995871bdd6cef28674eab66663
Félix Bouveret <felix@getluko.com>
2023-04-13 18:06:53 +0200

    fix: test

--- release.config.js
+++ release.config.js
@@ -5,16 +5,16 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     { '@semantic-release/changelog': { changelogFile: 'CHANGELOG.md' } },
     [
-      '@semantic-release/exec',
-      {
-        successCmd: 'node ./script/release/index.js "${nextRelease.version}"'
-      }
-    ],
-    [
       '@semantic-release/npm',
       {
         assets: ['package.json', 'app.config.js'],
         npmPublish: false
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        successCmd: 'node ./script/release/index.js "${nextRelease.version}"'
       }
     ],
     [
@@ -23,6 +23,7 @@ module.exports = {
         assets: ['CHANGELOG.md', 'package.json', 'app.config.js'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
-    ]
+    ],
+    '@semantic-release/github'
   ]
 };
